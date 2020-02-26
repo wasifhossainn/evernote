0c014ae4d3bee947a2e2e0950b61c60e73aa51ac
Wasif Hossain <wasif.hossain@hotmail.com>
2020-02-26 14:08:47 +1100

    Updates

---
+++ precache-manifest.6d97e19a9db12c99ef5b907da1cab753.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53e038c732e7dab7f757535591db42f5",
+    "url": "/wasifhossainn/evernote/index.html"
+  },
+  {
+    "revision": "f9cada01ed16a53685e8",
+    "url": "/wasifhossainn/evernote/static/css/main.475ab4aa.chunk.css"
+  },
+  {
+    "revision": "aca5630bd6bda08505b7",
+    "url": "/wasifhossainn/evernote/static/js/2.8872d3b0.chunk.js"
+  },
+  {
+    "revision": "f9cada01ed16a53685e8",
+    "url": "/wasifhossainn/evernote/static/js/main.d8d0a7bc.chunk.js"
+  },
+  {
+    "revision": "7fc1dfdd9302a2081648",
+    "url": "/wasifhossainn/evernote/static/js/runtime~main.a978c339.js"
+  }
+]);
